8297573e430d3f321e2025b025c65362e95912a6
ywalakamar <barxwells@gmail.com>
2023-07-06 18:06:44 +0300

    setup: initial commit

---
+++ src/test/java/com/edu/hogwartsartifactsonline/HogwartsArtifactsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.hogwartsartifactsonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtifactsOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
